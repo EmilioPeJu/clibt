f900d119064c1df5f07e7bcbf8f8ea6eb30eb8d3
hir12111 <hir12111@gmail.com>
2020-04-06 02:01:22 +0100

    Quit inside the run function

--- clibt/seqdig.py
+++ clibt/seqdig.py
@@ -19,7 +19,6 @@ class SeqDig(object):
         signal.signal(signal.SIGINT, self.signal_handle)
 
     def signal_handle(self, frame, sig):
-        self.quit()
         self.want_quit = True
 
     def draw(self):
@@ -30,6 +29,7 @@ class SeqDig(object):
         period_ms = 60000.0 / self.bpm
         duration_ms = self.duration * 1000
         tick()
+
         while tock() < duration_ms and not self.want_quit:
             wait_period(period_ms)
             self.lines = [get_random_digits(self.cols, self.base)
@@ -37,7 +37,6 @@ class SeqDig(object):
             self.ui.process_events()
             self.draw()
 
-    def quit(self):
         self.ui.quit()
 
 
@@ -45,7 +44,6 @@ def main():
     parameters = parse_args_from_func(SeqDig.__init__)
     ex = SeqDig(**parameters)
     ex.run()
-    ex.quit()
 
 
 if __name__ == "__main__":
